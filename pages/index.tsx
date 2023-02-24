bf07ad1f46f8d985b48fc09cc39cc1a77a130873
grantrisk <riskgrant@gmail.com>
2023-02-23 19:00:49 -0500

    updated nav bar for phone users

--- pages/index.tsx
+++ pages/index.tsx
@@ -74,43 +74,91 @@ export default function Home() {
             <div className={styles.location}>
               <ul>
                 <li>
-                  <p>7701 S Raeford Rd</p>
-                  <p>Fayetteville, NC 28304</p>
+                  <a
+                    href="https://www.google.com/maps?q=7701+S+Raeford+Rd%2C+Fayetteville%2C+NC+28304"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    <p>7701 S Raeford Rd</p>
+                    <p>Fayetteville, NC 28304</p>
+                  </a>
                   <a href="tel:9108260100">(910) 826-0100</a>
                 </li>
                 <li>
-                  <p>4601 Ramsey St</p>
-                  <p>Fayetteville, NC 28311</p>
+                  <a
+                    href="https://www.google.com/maps?q=4601+Ramsey+St%2C+Fayetteville%2C+NC+28311"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    <p>4601 Ramsey St</p>
+                    <p>Fayetteville, NC 28311</p>
+                  </a>
                   <a href="tel:9104884674">(910) 488-4674</a>
                 </li>
                 <li>
-                  <p>1550 Skibo Rd</p>
-                  <p>Fayetteville, NC 28303</p>
+                  <a
+                    href="https://www.google.com/maps?q=1550+Skibo+Rd%2C+Fayetteville%2C+NC+28303"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    <p>1550 Skibo Rd</p>
+                    <p>Fayetteville, NC 28303</p>
+                  </a>
                   <a href="tel:9109732366">(910) 973-2366</a>
                 </li>
                 <li>
-                  <p>250 Highlands Square Dr</p>
-                  <p>Hendersonville, NC 28792</p>
+                  <a
+                    href="https://www.google.com/maps?q=250+Highlands+Square+Dr%2C+Hendersonville%2C+NC+28792"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    <p>250 Highlands Square Dr</p>
+                    <p>Hendersonville, NC 28792</p>
+                  </a>
                   <a href="tel:8286967898">(828) 696-7898</a>
                 </li>
                 <li>
-                  <p>601 Souter Pl</p>
-                  <p>Fort Bragg, NC 28307</p>
+                  <a
+                    href="https://www.google.com/maps?q=601+Souter+Pl%2C+Fort+Bragg%2C+NC+28307"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    <p>601 Souter Pl</p>
+                    <p>Fort Bragg, NC 28307</p>
+                  </a>
                   <a href="tel:9104970082">(910) 497-0082</a>
                 </li>
                 <li>
-                  <p>3030 N Main St</p>
-                  <p>Hope Mills, NC 28348</p>
+                  <a
+                    href="https://www.google.com/maps?q=3030+N+Main+St%2C+Hope+Mills%2C+NC+28348"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    <p>3030 N Main St</p>
+                    <p>Hope Mills, NC 28348</p>
+                  </a>
                   <a href="tel:9104246413">(910) 424-6413</a>
                 </li>
                 <li>
-                  <p>2025 N Marine Blvd</p>
-                  <p>Jacksonville, NC 28546</p>
+                  <a
+                    href="https://www.google.com/maps?q=2025+N+Marine+Blvd%2C+Jacksonville%2C+NC+28546"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    <p>2025 N Marine Blvd</p>
+                    <p>Jacksonville, NC 28546</p>
+                  </a>
                   <a href="tel:9109371297">(910) 937-1297</a>
                 </li>
                 <li>
-                  <p>250 Turner St</p>
-                  <p>Aberdeen, NC 28315</p>
+                  <a
+                    href="https://www.google.com/maps?q=250+Turner+St%2C+Aberdeen%2C+NC+28315"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    <p>250 Turner St</p>
+                    <p>Aberdeen, NC 28315</p>
+                  </a>
                   <a href="tel:9106951034">(910) 695-1034</a>
                 </li>
               </ul>
